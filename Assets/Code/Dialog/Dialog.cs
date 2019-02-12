d20447472e5bf762074753c4dfad2d592242549b
VasilisNNN <VasilisNNN@users.noreply.github.com>
2019-02-12 23:47:22 +0200

    Анимация текста при диалоге и мелкие правки.

--- Assets/Code/Dialog/Dialog.cs
+++ Assets/Code/Dialog/Dialog.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Steamworks;
 
 public class Dialog : MonoBehaviour {
@@ -16,6 +18,9 @@ public class Dialog : MonoBehaviour {
 
 
 	private string[] texB;
+    private string[] texBScroll,AllText;
+    private float TextScrollTimer, TextScrollTimerMax;
+    private int LineEnd;
 	private int finalLine = 1;
 	private int finalLinePl = 1;
 	private int CorrentLine = 0;
@@ -44,8 +49,22 @@ public class Dialog : MonoBehaviour {
 
     public string AchivementName = "-";
 
+    public bool Indifferent;
+    public bool Exited;
+    public bool Anger;
+
+    private List<AudioClip> IndifferentClips = new List<AudioClip>();
+    private List<AudioClip> ExitedClips = new List<AudioClip>();
+    private List<AudioClip> AngerClips = new List<AudioClip>();
+
+    private AudioSource AUSource;
+    private bool cancelTyping, isTyping;
+    private char EmptyLines;
+    private float typeSpeed = 0.04f;
+
     private void Start () {
-       // MinDialogTime = -1;
+        
+        // MinDialogTime = -1;
         skin = Resources.Load<GUISkin> ("Invent/Slot");
 	   
 		if(GameObject.Find("Vasilis")!=null)
@@ -54,12 +73,54 @@ public class Dialog : MonoBehaviour {
         _mouse = GameObject.Find("Mouse(Clone)").GetComponent<Mouse>();
         if(FaceString.Length>1)
         Face = Resources.Load<Texture2D>("Pers/Portrey/" + FaceString);
+        if (Indifferent || Exited || Anger)
+        {
+            if (GetComponent<AudioSource>() == null) gameObject.AddComponent<AudioSource>();
+
+        }
+        TextScrollTimerMax = 0.1f;
+        TextScrollTimer = Time.fixedTime + TextScrollTimerMax;
+        AUSource = GetComponent<AudioSource>();
+        /* for (int i = 0; i < 4; i++)
+         {
+             IndifferentClips.Add(Resources.Load<AudioClip>("s" + i));
+
+         }*/
+        if (LinesRu != null && PlayerPrefs.GetInt("Language") == -1 && LinesRu.Count > 0)
+        {
+
+            if (PlayerPrefs.GetInt("Day") < LinesRu.Count) texB = LinesRu[PlayerPrefs.GetInt("Day")].line;
+            else texB = LinesRu[LinesRu.Count - 1].line;
+        }
+        if (LinesEn != null && PlayerPrefs.GetInt("Language") >= 0 && LinesEn.Count > 0)
+        {
+
+            if (PlayerPrefs.GetInt("Day") < LinesEn.Count) texB = LinesEn[PlayerPrefs.GetInt("Day")].line;
+            else texB = LinesEn[LinesEn.Count - 1].line;
+
+        }
+
+        texBScroll = new string[texB.Length];
+       
+        AllText = new string[texB.Length];
+
+        /*for (int j = 0; j < texB.Length; j++)
+        {
+            
+
+            for (int i = 0; i < texB[j].Length; i++)
+                texBScroll[j] += " ";
+            print(texBScroll[j]);
+        }*/
+
+
     }
-    
 
-	private void Update()
+
+    private void Update()
 	{
-
+       
+       
         if (_mouse.pointnclick)
         {
             enter = Input.GetMouseButtonDown(0);
@@ -90,39 +151,104 @@ public class Dialog : MonoBehaviour {
             else texB = LinesEn[LinesEn.Count-1].line;
             
         }
-		
-		if (CollisionCase) {
+
+        
+
+
+
+        if (CollisionCase) {
 
             if (coll_obj.Contains(gameObject))
             {
+                /* if (texBScroll.Length == 0)
+                 {
+                     texBScroll = null;
+                     for (int i = 0; i < texB[CorrentLine].Length; i++)
+                         texBScroll += " ";
+                 }*/
+            
 
-                if (enter && MinDialogTime < Time.fixedTime&& pl.DayFinish>=1&&texB!=null)
+                if (enter && pl.DayFinish>=1&&texB!=null)
                 {
 
-                    if (PlayIn && CorrentLine < texB.Length)
+                   
+
+                    
+                        if (PlayIn && CorrentLine < texB.Length)
+                        {
+
+                        if (NoEnter)
+                        {
+                            if (PlayIn && CorrentLine < texB.Length - 1)
+                            {
+                                if (MinDialogTime < Time.fixedTime)
+                                {
+                                    CorrentLine++;
+                                    MinDialogTime = Time.fixedTime + 0.2f;
+                                }
+                            }
+                        }
+                        else
+                        {
+
+                            if (texBScroll[CorrentLine].Length >= texB[CorrentLine].Length)
+                            {
+                                CorrentLine++;
+                                //MinDialogTime = Time.fixedTime + 0.2f;
+                            }
+                            // else LineEnd = texB[CorrentLine].Length;
+
+                        }
+
+                        }
+
+                    if (!NoEnter)
                     {
-                        CorrentLine++;
-                        MinDialogTime = Time.fixedTime + 0.2f;
+                        if (!isTyping)
+                        {
+                            if (CorrentLine <= texB.Length - 1)
+                            {
+                                StartCoroutine(TextScroll(texB[CorrentLine]));
+                                // MinDialogTime = Time.fixedTime + 0.2f;
+                            }
+                        }
+                        else if (isTyping && !cancelTyping)
+                        {
+                            cancelTyping = true;
+
+                        }
                     }
 
-                    if (!PlayIn)
-                    {
-                        PlayIn = true;
-                        MinDialogTime = Time.fixedTime + 0.2f;
-                    }
 
-                    if (PlayIn && CorrentLine == texB.Length)
-                    {
-                        PlayIn = false;
 
-                        CorrentLine = 0;
-                        MinDialogTime = Time.fixedTime + 0.2f;
-                    }
 
+                        if (MinDialogTime < Time.fixedTime)
+                        {
+
+                            if (!PlayIn)
+                            {
+                                PlayIn = true;
+
+                                MinDialogTime = Time.fixedTime + 0.2f;
+                            }
+
+
+                            if (PlayIn && CorrentLine == texB.Length)
+                            {
+                                PlayIn = false;
+
+                                CorrentLine = 0;
+                                MinDialogTime = Time.fixedTime + 0.2f;
+                            }
+                        }
+                    
                 }
 
                 if (NoEnter)
                 {
+                   
+                    texBScroll[CorrentLine] = texB[CorrentLine];
+                    
                     if (PlayOnes)
                     {
                         if (PlayerPrefs.GetInt(name + SceneManager.GetActiveScene().name) != 1)
@@ -162,20 +288,25 @@ public class Dialog : MonoBehaviour {
     // Update is called once per frame
     private void OnGUI()
     {
-        if (pl.DayFinish>1.1f&& texB!=null) { 
-        /*if (LinesRu != null && PlayerPrefs.GetInt("Language") == -1)
-        {
+        if (pl.DayFinish>1.1f&& texB!=null) {
+            /*if (LinesRu != null && PlayerPrefs.GetInt("Language") == -1)
+            {
 
-            if (PlayerPrefs.GetInt("Day") < LinesRu.Count) texB = LinesRu[PlayerPrefs.GetInt("Day")].line;
-            else texB = LinesRu[LinesRu.Count - 1].line;
-        }
-        if (LinesEn != null && PlayerPrefs.GetInt("Language") == 1)
-        {
+                if (PlayerPrefs.GetInt("Day") < LinesRu.Count) texB = LinesRu[PlayerPrefs.GetInt("Day")].line;
+                else texB = LinesRu[LinesRu.Count - 1].line;
+            }
+            if (LinesEn != null && PlayerPrefs.GetInt("Language") == 1)
+            {
 
-            if (PlayerPrefs.GetInt("Day") < LinesEn.Count) texB = LinesEn[PlayerPrefs.GetInt("Day")].line;
-            else texB = LinesEn[LinesEn.Count - 1].line;
+                if (PlayerPrefs.GetInt("Day") < LinesEn.Count) texB = LinesEn[PlayerPrefs.GetInt("Day")].line;
+                else texB = LinesEn[LinesEn.Count - 1].line;
 
-        }*/
+            }*/
+          /*  if (!PlayIn)
+            {
+                TextScrollTimer = Time.fixedTime + TextScrollTimerMax;
+             
+            }*/
 
         if (PlayIn == true && CorrentLine < texB.Length)
         {
@@ -221,32 +352,86 @@ public class Dialog : MonoBehaviour {
 	}
     void PosM(float stringslength)
     {
-        if ((19 * (stringslength / 16)) >= 75)
-            fheight = 35 + 19 * stringslength / 16;
+        if ((19 * (stringslength / 20)) >= 75)
+            fheight = 35 + 19 * stringslength / 20;
         else
             fheight = 140;
-        fwidth = 30 * 10;
-        
+
+        if (!VasilisMind)
+        {
+
+            if (stringslength < 17)
+                fwidth = 30 * 8;
+            else
+                fwidth = 30 * 15;
+        }
+        else fwidth = 300;
+
 
     }
    
     void DrawLines()
     {
         if (AchivementName.Length > 1) SetACH(AchivementName);
+        if (!VasilisMind)
+        {
+            if (Anger)
+            {
+                AUSource.clip = AngerClips[Random.Range(0, AngerClips.Count)];
+                AUSource.loop = true;
+                if(!AUSource.isPlaying) AUSource.Play();
+            }
+
+        }
 
 
         if (texB.Length > 0)
         {
+            
+           
+
             if (GetComponent<AudioSource>() != null && GetComponent<Trigger>() == null)
             {
                 if(!GetComponent<AudioSource>().isPlaying) GetComponent<AudioSource>().Play();
             }
 
             PosM(texB[PlayerPrefs.GetInt(name + "Dialog")].Length);
+           // int t = 0;
 
             if (CorrentLine <= texB.Length - 1)
             {
-                GUI.Box(rectlable, texB[CorrentLine], skin.customStyles[SkinNum]);
+
+
+
+                /* if (t <= EmptyLines.Length - 1)
+                 {
+                     if (texBScroll.Length< EmptyLines.Length)
+                         texBScroll[t]+= EmptyLines[t];
+                 }*/
+
+                /* if (TextScrollTimer < Time.fixedTime)
+                 {
+
+
+                      if(LineEnd == texB[CorrentLine].Length)
+                          texBScroll[CorrentLine] = texB[CorrentLine];
+
+                      if (texB[CorrentLine].Length > 0 && LineEnd < texB[CorrentLine].Length)
+                      {
+                          texBScroll[CorrentLine].PadLeft(LineEnd);
+                          texBScroll[CorrentLine] += texB[CorrentLine][LineEnd];
+                      }
+
+                      if (texB[CorrentLine].Length > 0 && LineEnd < texB[CorrentLine].Length)
+                      LineEnd++;
+
+
+
+
+                     TextScrollTimer = Time.fixedTime + TextScrollTimerMax;
+                 }*/
+                //texBScroll.
+                GUI.Box(rectlable, texBScroll[CorrentLine], skin.customStyles[SkinNum]);
 
                 if (WrightInJournal && PlayerPrefs.GetInt(name + SceneManager.GetActiveScene().name + "InJournal") == 0)
                 {
@@ -286,7 +471,55 @@ public class Dialog : MonoBehaviour {
 
 
     }
+    private IEnumerator TextScroll(string LineOfText)
+    {
+        int leter = 0;
+        int leter2 = 0;
+        texBScroll[CorrentLine] = "";
+        AllText[CorrentLine] = "";
+        isTyping = true;
+        cancelTyping = false;
+        int w = 0;
+        int w2 = 0;
+        string s = " ";
+      
+        while (leter2 < LineOfText.Length)
+        {
+            if (w2 < 40)
+            {
+                AllText[CorrentLine] += LineOfText[leter2];
+                w2++;
+            }
+            if (w2 > 30 && LineOfText.ToCharArray()[leter2].Equals(s.ToCharArray()[0]))
+            {
+                AllText[CorrentLine] += "\n";
+                w2 = 0;
+            }
 
+            leter2++;
+        }
+        while (!cancelTyping && isTyping&&(leter< LineOfText.Length))
+        {
+            if (w <40)
+            {
+                texBScroll[CorrentLine] += LineOfText[leter];
+                w++;
+            }
+
+            if(w >30&&LineOfText.ToCharArray()[leter].Equals(s.ToCharArray()[0]))
+            {
+                texBScroll[CorrentLine] += "\n";
+                w = 0;
+            }
+
+            leter++;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+
+        texBScroll[CorrentLine] = AllText[CorrentLine];
+        isTyping = false;
+        cancelTyping = false;
+    }
 
     void Save()
     {
@@ -316,10 +549,10 @@ public void SetDialogPartName(string PL)
         }
     }
 
-    public int GetFinalLine()
+   /* public int GetFinalLine()
 	{
 		return finalLine;
-	}
+	}*/
 
 
 public void SetTextField(Rect Field)
